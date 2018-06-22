8befbe73e614bb19fad6338aab77f5f335bc5626
Code Cadet <codecadet@ritchie.local>
2018-06-22 18:26:37 +0000

    changes Garagem-c

--- Garagem/src/org/academiadecodigo/bootcamp/c/Garagem.java
+++ Garagem/src/org/academiadecodigo/bootcamp/c/Garagem.java
@@ -2,22 +2,79 @@ package org.academiadecodigo.bootcamp.c;
 
 public class Garagem {
 
+    private Vehicle[] spots;
+    private int freeSpots;
+
+
+    public Garagem(int spots) {
+
+        this.spots = new Vehicle[spots];
+        freeSpots = this.spots.length;
+
+    }
+
+    public boolean park() {
+
+
+        for (int i = 0; i < spots.length; i++) {
+
+            if (spots[i]){
+                continue;
+            }
+
+
+            spots[i] = true;
+            freeSpots--;
+            return true;
+
+        }
+
+        return false;
+
+    }
+
+
+    public boolean unpark(int spotsNumber) {
+
+        if (spotsNumber > spots.length - 1) {
+            return false;
+        }
+
+        if (!spots[spotsNumber]) {
+            return false;
+
+        }
+
+        spots[spotsNumber] = false;
+        freeSpots++;
+        return true;
+
+    }
+    public int getCappacity() {
+        return spots.length;
+    }
+
+    public int getFreeSpots() {
+        return freeSpots;
+    }
+
+
     /**
      * Este método é apenas para impressao do estado da garagem (para ajudar)
      * Descomentar quando a classe estiver implementada
      * Assume a existencia de uma propriedade `spots`
      */
-    @Override
+ /*  @Override
     public String toString() {
         String garageRepresentation = "";
 
-        /*
+
         for (int i = 0; i < spots.length; i++) {
             garageRepresentation += "|" + (spots[i] != null ? spots[i].getBrand().substring(0, 3) : " " + i + " ");
         }
         garageRepresentation += "|";
-        */
 
-        return garageRepresentation;
+
+        return garageRepresentation; */
     }
 }
--- Garagem/src/org/academiadecodigo/bootcamp/c/Vehicle.java
+++ Garagem/src/org/academiadecodigo/bootcamp/c/Vehicle.java
@@ -2,4 +2,38 @@ package org.academiadecodigo.bootcamp.c;
 
 public class Vehicle {
 
+    private String brand;
+    private int parkingSpot;
+
+
+    public Vehicle(String brand) {
+        this.brand = brand;
+
+    }
+
+    public void park(int parkingSpot) {
+        this.parkingSpot = parkingSpot;
+    }
+
+    public void unpark() {
+        this.parkingSpot = -1;
+
+    }
+
+    public boolean isparked() {
+
+        return parkingSpot != -1;
+
+    }
+
+
+    public int getParkingSpot() {
+        return parkingSpot;
+
+
+    }
+
+    public String getBrand() {
+        return brand;
+    }
 }
